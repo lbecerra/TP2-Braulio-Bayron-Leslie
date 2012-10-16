fd4215a0e72b8d666e7654bc111473d72d2b4c82
leslie.becerra <les9217@gmail.com>
2012-10-16 14:57:33 -0600

    Base conocimientos

---
+++ Le_Poulet.pl
@@ -0,0 +1,13 @@
+receta(a,b,c,pollo,e,f).
+receta(pizza1,armando3,italiana,salsaaaa,2,comprar).
+receta(pizza1,armando,italiana,queso,10,moler).
+receta(pizza1,armando,italiana,jamon,15,tomas).
+receta(pizza1,armando,italiana,chile,2,picar).
+receta(pizza1,armando,italiana,pasta,2,revolver).
+receta(pizza1,armando,italiana,salsa,2,comprar).
+receta(pizza1,armando4,italiana,sssssalsa,2,comprar).
+receta(pizza1,armando2,italiana,pasta,2,revolver).
+receta(pizza1,armando2,italiana,salsa,2,comprar).
+receta(papasf,yo,italiana,pollo,1,trozar).
+receta(coca,armando,italiana,tomate,3,picar).
+receta(lasagna,armando,italiana,tomate,3,picar).
